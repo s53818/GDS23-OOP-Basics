12a125e0fe6351af36dc332f06fc63f2c402a79c
Sergio Cebular <s53818@edu.campus02.at>
2023-10-14 08:06:16 +0200

    OOP 1

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,7 @@
 public class Car {
-    
+    // Datentyp Car erstellt
+    public String color;
+    public int countDoors;
+    public int mileage;
+    public String name;
 }
---
+++ src/DemoApp.java
@@ -0,0 +1,20 @@
+public class DemoApp {
+    public static void main(String[] args) {
+
+        //Datentyp Car
+        Car vwGolf8 = new Car();
+        // Variablen Werte zuweisen
+        // Attribute kann man sich wie Variablen vorstellen
+        // String color = "Black"; im Hintergrund, nur dass der Datentyp Car ist
+        vwGolf8.color = "Black";
+        vwGolf8.countDoors=5;
+        vwGolf8.name="Hurbey";
+        vwGolf8.mileage=234_211;
+
+        Car audiA3 = new Car();
+        audiA3.color="Red";
+        audiA3.countDoors=3;
+        audiA3.name="Audi";
+        audiA3.mileage=111_120;
+    }
+}
